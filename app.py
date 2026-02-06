2436620186ed2966504fd822719049f3365280ca
Bigillow <tsunheil@nycstudents.net>
2026-02-06 09:26:23 -0500

    Dictionary

---
+++ app.py
@@ -0,0 +1,6 @@
+item = {
+    "name": "ASUS TUF Gaming Radeon RX 9070 XT OC Edition 16GB GDDR6",
+    "price": 849.99,
+    "department": "Electronics",
+    "description": "The ASUS TUF Gaming Radeon RX 9070 XT OC Edition 16GB GDDR6 (Model: TUF-RX9070XT-O16G-GAMING) is a high-performance graphics card built on the RDNA 4 architecture, offering 16GB GDDR6 VRAM, excellent cooling with triple Axial-tech fans, and enhanced ray tracing."
+}
